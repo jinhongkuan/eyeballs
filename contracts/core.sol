833b5478709f3bb00a64e8d813554abd644acacf
kingw18 <liamkinghome@gmail.com>
2023-09-23 14:39:40 -0400

    New Core Contract

--- contracts/core.sol
+++ contracts/core.sol
@@ -1,30 +1,81 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
+
+import {ByteHasher} from "./helpers/ByteHasher.sol";
+import {IWorldID} from "./interfaces/IWorldID.sol";
 
 contract MyContract {
-    mapping(uint256 => bool) private hashToBool;
+    using ByteHasher for bytes;
+
+    IWorldID internal immutable worldId;
+    uint256 internal immutable externalNullifierHash;
+    uint256 internal immutable groupId = 1;
+
+    mapping(uint256 => bool) internal nullifierHashes;
+    mapping(uint256 => bool) internal nullifierHashAndURL;
     mapping(uint256 => uint256) private balance;
 
-    function addURLHash(uint256 nullifierHash, string memory URL) external {
-        uint256 hash = uint256(keccak256(abi.encodePacked(nullifierHash, URL)));
-        hashToBool[hash] = true;
+    event BalanceUpdated(uint256 indexed nullifierHash, uint256 newBalance);
+    event ViewedStatusUpdated(uint256 indexed hash, bool status);
+
+    constructor(
+        IWorldID _worldId,
+        string memory _appId,
+        string memory _actionId
+    ) {
+        worldId = _worldId;
+        externalNullifierHash = abi
+            .encodePacked(abi.encodePacked(_appId).hashToField(), _actionId)
+            .hashToField();
     }
 
-    function addNullifierHash(uint256 nullifierHash, uint256 tokens) external {
-        balance[nullifierHash] = tokens;
+    // Explicit function visibility for clarity
+    function getHash(
+        uint256 nullifierHash,
+        string memory url
+    ) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(nullifierHash, url)));
     }
 
-    function checkViewed(uint256 hash) internal view returns (bool) {
-        return hashToBool[hash];
+    function addBalance(uint256 nullifierHash, uint256 tokens) internal {
+        // Implement your access control logic here
+
+        balance[nullifierHash] += tokens;
+        emit BalanceUpdated(nullifierHash, balance[nullifierHash]);
     }
 
     function getBalance(uint256 nullifierHash) external view returns (uint256) {
         return balance[nullifierHash];
     }
 
-    function decrementBalance(uint256 nullifierHash) external {
-        if (!checkViewed(nullifierHash)) {
-            balance[nullifierHash] -= 1;
+    function decrementBalance(uint256 nullifierHash) internal {
+        balance[nullifierHash] = balance[nullifierHash] - 100; // Underflow protection by Solidity 0.8.x
+        emit BalanceUpdated(nullifierHash, balance[nullifierHash]);
+    }
+
+    function verifyAndExecute(
+        address signal,
+        uint256 root,
+        uint256 nullifierHash,
+        string memory url,
+        uint256[8] calldata proof
+    ) public {
+        if (!nullifierHashes[nullifierHash]) {
+            nullifierHashes[nullifierHash] = true;
+            addBalance(nullifierHash, 1000);
+        }
+        worldId.verifyProof(
+            root,
+            groupId,
+            abi.encodePacked(signal).hashToField(),
+            nullifierHash,
+            externalNullifierHash,
+            proof
+        );
+        uint256 hash = getHash(nullifierHash, url);
+        if (!nullifierHashAndURL[hash]) {
+            decrementBalance(nullifierHash);
+            nullifierHashAndURL[hash] = true;
         }
     }
 }
