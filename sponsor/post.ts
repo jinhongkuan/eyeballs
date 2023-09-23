2cf09ea938e355bfae7b0a9289e5a11aa446af15
jinhongkuan <kuanjh123@gmail.com>
2023-09-23 13:04:34 -0400

    dummy paymaster server

---
+++ sponsor/post.ts
@@ -0,0 +1,17 @@
+import { TransactionRequest, ethers } from "ethers";
+
+const ARBITRIUM_RPC = process.env.ARBITRIUM_RPC; 
+
+// Create transaction request to call on-chain function 
+// const constructTransactionFromPostRequest = (): TransactionRequest => {
+
+// }
+
+async function routes (fastify, options) {
+    fastify.get('/', async (request, reply) => {
+      // Construct and sign transaction 
+      return { hello: 'world' }
+    })
+  }
+  
+  module.exports = routes
