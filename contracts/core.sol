d24fdb1da4dc8966db68feebb556592d2fbc2a07
kingw18 <liamkinghome@gmail.com>
2023-09-23 17:09:31 -0400

    Just cleaning it up

--- contracts/core.sol
+++ contracts/core.sol
@@ -7,6 +7,12 @@ import {IWorldID} from "./interfaces/IWorldID.sol";
 contract MyContract {
     using ByteHasher for bytes;
 
+    // Constants for magic numbers
+    uint256 private constant INITIAL_BALANCE = 1000;
+    uint256 private constant BASE_COST = 100;
+    uint256 private constant REFERRER_COST = 90;
+    uint256 private constant REFERRER_BONUS = 70;
+    
     IWorldID internal immutable worldId;
     uint256 internal immutable externalNullifierHash;
     uint256 internal immutable groupId = 1;
@@ -18,6 +24,7 @@ contract MyContract {
     event BalanceUpdated(uint256 indexed nullifierHash, uint256 newBalance);
     event ViewedStatusUpdated(uint256 indexed hash, bool status);
 
+    // Constructor
     constructor(
         IWorldID _worldId,
         string memory _appId,
@@ -29,7 +36,7 @@ contract MyContract {
             .hashToField();
     }
 
-    // Explicit function visibility for clarity
+    // Get hash for nullifier and URL
     function getHash(
         uint256 nullifierHash,
         string memory url
@@ -37,33 +44,36 @@ contract MyContract {
         return uint256(keccak256(abi.encodePacked(nullifierHash, url)));
     }
 
+    // Add balance for a given nullifier hash
     function addBalance(uint256 nullifierHash, uint256 tokens) internal {
-        // Implement your access control logic here
-
         balance[nullifierHash] += tokens;
         emit BalanceUpdated(nullifierHash, balance[nullifierHash]);
     }
 
+    // Public function to get balance for a nullifier hash
     function getBalance(uint256 nullifierHash) external view returns (uint256) {
         return balance[nullifierHash];
     }
 
+    // Decrease balance for a given nullifier hash
     function decrementBalance(uint256 nullifierHash, uint256 amount) internal {
-        balance[nullifierHash] = balance[nullifierHash] - amount; // Underflow protection by Solidity 0.8.x
+        require(balance[nullifierHash] >= amount, "Insufficient balance");
+        balance[nullifierHash] -= amount;
         emit BalanceUpdated(nullifierHash, balance[nullifierHash]);
     }
 
+    // Main function to verify and execute logic
     function verifyAndExecute(
-        string memory signal,
+        string memory signal, // This is the url of the hosted content
         uint256 root,
         uint256 nullifierHash,
         uint256[8] calldata proof,
         uint256 referrerHash
     ) public {
-        uint256 cost = 100;
+        uint256 cost = BASE_COST;
         if (!nullifierHashes[nullifierHash]) {
             nullifierHashes[nullifierHash] = true;
-            addBalance(nullifierHash, 1000);
+            addBalance(nullifierHash, INITIAL_BALANCE);
         }
 
         worldId.verifyProof(
@@ -80,8 +90,8 @@ contract MyContract {
             if (referrerHash != 0) {
                 uint256 referrerUrlHash = getHash(referrerHash, signal);
                 if (nullifierHashAndURL[referrerUrlHash]) {
-                    cost = 90;
-                    addBalance(referrerUrlHash, 70);
+                    cost = REFERRER_COST;
+                    addBalance(referrerUrlHash, REFERRER_BONUS);
                 }
             }
             decrementBalance(nullifierHash, cost);
