ad81354f9f288c0f8df991760d78234def1b5988
alexbckr <amb3yn@virginia.edu>
2023-09-23 22:08:28 -0400

    more g

--- example-site/src/Eyewall.tsx
+++ example-site/src/Eyewall.tsx
@@ -4,7 +4,7 @@ import { CredentialType, IDKitWidget } from "@worldcoin/idkit";
 import "../src/App.css";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { urlToAddressBytes } from "../src/stringToAddress";
+import { urlToAddressBytes } from "./stringToAddress";
 import React from "react";
 
 const glowHeight = 200;
@@ -89,6 +89,7 @@ export function Eyewall() {
   const cardRef = useRef<HTMLDivElement>(null);
   const [boxPosition, setBoxPosition] = useState({ x: 0, y: 0 });
   const [boxVisible, setBoxVisible] = useState(false);
+  const [shouldClose, setShouldClose] = useState(false);
 
   useEffect(() => {
     setOpen(true);
@@ -150,15 +151,20 @@ export function Eyewall() {
   }, []);
 
   const handleSuccess = async (data: any) => {
-    await fetch(`http://${sponsorAddress}/post`, {
-      method: "POST",
-      body: JSON.stringify(data),
-    });
     console.log("data", data);
     setOpen(false);
   };
   const handleVerify = (data: any) => {
     console.log("verify");
+    setShouldClose(true);
+
+    fetch(`http://${sponsorAddress}/post`, {
+      method: "POST",
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log("done", res);
+      setOpen(false);
+    });
   };
   const signal = urlToAddressBytes(window.location.href);
 
@@ -182,19 +188,23 @@ export function Eyewall() {
                 </p>
               </Center>
               <p>This costs 1 view.</p>
-              <IDKitWidget
-                app_id="app_staging_9b5d49b869afa5618a88c00937987526" // obtained from the Developer Portal
-                action="open" // this is your action name from the Developer Portal
-                onSuccess={(data: any) => handleSuccess(data)} // callback when the modal is closed
-                signal={signal}
-                handleVerify={(data: any) => handleVerify(data)} // optional callback when the proof is received
-                credential_types={["orb"] as CredentialType[]} // optional, defaults to ['orb']
-                enableTelemetry // optional, defaulsts to false
-              >
-                {({ open }) => (
-                  <button onClick={open}>Verify with World ID and view</button>
-                )}
-              </IDKitWidget>
+              {!shouldClose && (
+                <IDKitWidget
+                  app_id="app_staging_9b5d49b869afa5618a88c00937987526" // obtained from the Developer Portal
+                  action="open" // this is your action name from the Developer Portal
+                  onSuccess={(data: any) => handleSuccess(data)} // callback when the modal is closed
+                  signal={signal}
+                  handleVerify={(data: any) => handleVerify(data)} // optional callback when the proof is received
+                  credential_types={["orb"] as CredentialType[]} // optional, defaults to ['orb']
+                  enableTelemetry // optional, defaulsts to false
+                >
+                  {({ open }) => (
+                    <button onClick={open}>
+                      Verify with World ID and view
+                    </button>
+                  )}
+                </IDKitWidget>
+              )}
               <Space h="md" />
               <Center>
                 <Link to="/">Home</Link>
